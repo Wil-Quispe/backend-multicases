0ce124341a32f8613296456874ce296650186cbd
Wil-Quispe <newwilcode@gmail.com>
2022-07-15 18:50:07 -0500

    create mutation signup

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -7,6 +7,25 @@ const resolvers = {
       return users
     },
   },
+  Mutation: {
+    signup: async (
+      _: any,
+      { name, username, latitude, longitude }: any,
+      context: any
+    ) => {
+      const { Models } = context
+      const { User } = Models
+
+      const newUser = new User({
+        name,
+        username,
+        currentLocation: { latitude, longitude },
+        lastLocation: { latitude: '0', longitude: '0' },
+      })
+
+      return await newUser.save()
+    },
+  },
 }
 
 export default resolvers
