a52fe2691cf3d44aaef5fc504f76c62005bc6a8a
Wil-Quispe <newwilcode@gmail.com>
2022-07-15 18:07:25 -0500

    query all users

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import { connect } from 'mongoose'
 export const connectDB = async () => {
   try {
     await connect(
-      'mongodb://uusseerr11:uusseerr11@cluster0-shard-00-00.odw98.mongodb.net:27017,cluster0-shard-00-01.odw98.mongodb.net:27017,cluster0-shard-00-02.odw98.mongodb.net:27017/?ssl=true&replicaSet=atlas-10t3ev-shard-0&authSource=admin&retryWrites=true&w=majority'
+      'mongodb://uusseerr11:uusseerr11@cluster0-shard-00-00.odw98.mongodb.net:27017,cluster0-shard-00-01.odw98.mongodb.net:27017,cluster0-shard-00-02.odw98.mongodb.net:27017/DBmulticases?ssl=true&replicaSet=atlas-10t3ev-shard-0&authSource=admin&retryWrites=true&w=majority'
     )
     console.log('DB connected')
   } catch (error) {
--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,6 +1,11 @@
 const resolvers = {
   Query: {
-    hello: () => 'holamundo',
+    user: async (_: any, __: any, context: any) => {
+      const { Models } = context
+      const { User } = Models
+      const users = await User.find({})
+      return users
+    },
   },
 }
 
